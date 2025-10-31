1e60371adba8440d5555467ceb093cf2c763d2dc
Ulrich Seul <ulrich.seul@gip.com>
2025-10-31 19:40:41 +0100

    No python installation in prod image

--- lib_scripts/os/install_additional_pkgs_no_python.sh
+++ lib_scripts/os/install_additional_pkgs_no_python.sh
@@ -45,6 +45,7 @@ if [[ ${OS_IMAGE} == oraclelinux:* ]]; then
 elif [[ ${OS_IMAGE} == redhat/ubi*:* ]]; then
     echo "No additional packages needed"
 elif [[ ${OS_IMAGE} == ubuntu:* ]]; then
+    echo "Going to install additional packages (no python)"
     apt --no-install-recommends -y update
     apt -y upgrade
     apt-get -y install wget xinetd net-tools bind9utils vim-tiny less libxml2-utils gnupg ca-certificates curl gcc systemd uuid-runtime

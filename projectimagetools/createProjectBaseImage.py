f62b4a956e3f6a0d4ca95cf8d973ba445dfda515
Ulrich Seul <ulrich.seul@gip.com>
2025-11-27 16:54:43 +0100

    code review

--- projectimagetools/createProjectBaseImage.py
+++ projectimagetools/createProjectBaseImage.py
@@ -20,6 +20,7 @@ import subprocess
 import sys
 import os
 import configparser
+import argparse
 
 SCRIPT_VERSION='0.0.1'
 TMP_CONTAINER_NAME='tmp.xyna.container.0'
@@ -212,6 +213,178 @@ class XynaAppList:
 ### end of class declaration
 
 
+class DockerContainer:
+  def __init__(self, image_name: str):
+    self.image_name = image_name
+    self.container_name = TMP_CONTAINER_NAME
+
+  def does_container_exist(self) -> bool:
+    result = subprocess.run(["docker", "container", "inspect", self.container_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    return result.returncode == 0
+
+
+  def start_xyna_base_container(self):
+    if self.does_container_exist():
+      self.drop_xyna_base_container()
+    result = subprocess.run(["docker", "run", "-d", "--name", self.container_name, self.image_name])
+    result.check_returncode()
+
+  def drop_xyna_base_container(self):
+    result = subprocess.run(["docker", "stop", self.container_name])
+    result = subprocess.run(["docker", "rm", self.container_name])
+    result.check_returncode()
+
+  def get_app_file_list_string(self) -> str:
+    docker_cmd=['docker', 'exec', '-t', self.container_name, 'bash', '-c', "find /tmp/XynaBlackEdition/components -iname \\*.app "]
+    app_paths=subprocess.check_output(docker_cmd, shell=True, text=True)
+    return app_paths
+
+  def get_installed_app_names(self) -> str:
+    docker_cmd=['docker', 'exec', '-t', self.container_name, 'bash', '-c', """awk -F= '($1=="installation.folder") { print $2 }'  /etc/opt/xyna/environment/black_edition_001.properties"""]
+    xyna_path = subprocess.check_output(docker_cmd, shell=True, text=True)
+    xyna_path = xyna_path.strip()
+    docker_cmd=['docker', 'exec', '-t', self.container_name, 'bash', '-c', "cd " + xyna_path + "/server; ./xynafactory.sh status; while [[ $? -ne 0 ]]; do sleep 1s; ./xynafactory.sh status; done"]
+    subprocess.run(docker_cmd, shell=True, text=True, check=False)
+    docker_cmd=['docker', 'exec', '-t', self.container_name, 'bash', '-c', xyna_path + "/server/xynafactory.sh listapplications | awk '(NR>1) { print $1 }'"]
+    app_names_str = subprocess.check_output(docker_cmd, shell=True, text=True)
+    app_names_str = app_names_str.replace("'", "")
+    app_names = app_names_str.splitlines()
+    return app_names
+### end of class declaration
+
+
+# !!!
+# To add a new section of properties to the generated property-file,
+# and correspondinly new command blocks to the generated Dockerfile:
+# Create a new child class of this class (similar to the AppSection-class below),
+# and add an instance of that new class in the Builder.init_sections()-method below
+# !!!
+class Section:
+  def add_to_properties(self, config: configparser.ConfigParser):
+    pass
+  def build_dockerfile_commands(self, config: configparser.ConfigParser) -> str:
+    pass
+### end of class declaration
+
+
+class AppSection(Section):
+  def __init__(self, container: DockerContainer = None):
+    self.container = container
+
+  def add_to_properties(self, config: configparser.ConfigParser):
+    if self.container is None:
+      raise Exception("Docker container not found")
+    config[SECTION_NAME_APPS] = {}
+    config.set(SECTION_NAME_APPS, PROP_FILE_APP_BLOCK_START)
+    pathstr = self.container.get_app_file_list_string()
+    apps = self.build_app_dict_from_paths(pathstr)
+    config[SECTION_NAME_APP_LIST] = apps
+
+  def build_dockerfile_commands(self, config: configparser.ConfigParser) -> str:
+    applist = XynaAppList(config = config)
+    apps = self.build_apps_to_install_string(applist)
+    return CMDS_APPS_TEMPL.replace("###_APP_CMD_LIST_###", apps)
+
+  def build_app_dict_from_paths(self, app_paths: str) -> dict:
+    applist = XynaAppList(paths = app_paths)
+    filter_app_names = self.container.get_installed_app_names()
+    applist.filter_apps(filter_app_names)
+    return applist.to_dict()
+
+  def build_apps_to_install_string(self, applist: XynaAppList) -> str:
+    ret = ""
+    isfirst = True
+    for app in applist.applist:
+      if not app.install_flag:
+        continue
+      if isfirst:
+        isfirst = False
+      else:
+        ret += " && \\  \n"
+      ret += APP_LINE + app.name
+    if isfirst:
+      ret += "    echo 'no apps to install'"
+    ret += " && \\"
+    return ret
+### end of class declaration
+
+
+class Builder:
+  def __init__(self, container: DockerContainer = None):
+    self.container = container
+    self.sections = self.init_sections()
+
+  def init_sections(self) -> list[Section]:
+    ret = []
+    ret.append(AppSection(self.container))
+    # !!!
+    # append new sections here
+    # !!!
+    return ret
+### end of class declaration
+
+
+class PropertyFileBuilder(Builder):
+  def __init__(self, image_name: str):
+    super().__init__(DockerContainer(image_name))
+    self.image_name = image_name
+
+  def execute(self, out_file: str):
+    print("Loading xyna base docker image to determine applications list...", flush=True)
+    self.container.start_xyna_base_container()
+    try:
+      content = self.gen_prop_content()
+    except:
+      print("Unexpected error:", sys.exc_info()[0])
+      self.container.drop_xyna_base_container()
+      raise
+    self.container.drop_xyna_base_container()
+    write_config_file(out_file, content)
+
+  def gen_prop_content(self) -> configparser.ConfigParser:
+    config = configparser.ConfigParser(allow_no_value=True)
+    config.optionxform = lambda option: option
+    firstline = PROP_FILE_HEADER.replace("###_BASE_IMAGE_###", self.image_name)
+    config[SECTION_NAME_HEADER] = {}
+    config.set(SECTION_NAME_HEADER, firstline)
+    config.set(SECTION_NAME_HEADER, PROP_NAME_BASE_IMAGE, self.image_name)
+    config.set(SECTION_NAME_HEADER, PROP_NAME_TARGET_IMAGE, PROP_VALUE_TARGET_IMAGE)
+    for section in self.sections:
+      section.add_to_properties(config)
+    return config
+### end of class declaration
+
+
+class DockerfileBuilder(Builder):
+  def __init__(self):
+    super().__init__()
+
+  def execute(self, prop_file: str, docker_file: str):
+    content = self.gen_docker_file_content(prop_file)
+    write_file(docker_file, content)
+
+  def gen_docker_file_content(self, prop_file: str) -> str:
+    config = configparser.ConfigParser(allow_no_value=True, comment_prefixes=(';'))
+    config.optionxform = lambda option: option
+    config.read(prop_file)
+    header = config[SECTION_NAME_HEADER]
+    base_image = header[PROP_NAME_BASE_IMAGE]
+    target_image = header[PROP_NAME_TARGET_IMAGE]
+    commands = self.gen_commands_string(config)
+    ret = DOCKER_TEMPLATE
+    ret = ret.replace("###_BASE_IMAGE_###", base_image)
+    ret = ret.replace("###_TARGET_IMAGE_###", target_image)
+    ret = ret.replace("###_ALL_COMMANDS_###", commands)
+    return ret
+
+  def gen_commands_string(self, config: configparser.ConfigParser) -> str:
+    commands = ""
+    for section in self.sections:
+      cmd = section.build_dockerfile_commands(config)
+      commands += cmd
+    return commands
+### end of class declaration
+
 
 def read_file(name: str) -> str:
   with open(name) as f:
@@ -228,161 +401,24 @@ def write_config_file(name: str, config: configparser.ConfigParser):
     config.write(f)
 
 
-def does_container_exist(name: str) -> bool:
-  result = subprocess.run(["docker", "container", "inspect", name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-  return result.returncode == 0
-
-
-def start_xyna_base_container(image_name: str):
-  if does_container_exist(TMP_CONTAINER_NAME):
-    drop_xyna_base_container()
-  result = subprocess.run(["docker", "run", "-d", "--name", TMP_CONTAINER_NAME, image_name])
-  result.check_returncode()
-
-
-def drop_xyna_base_container():
-  result = subprocess.run(["docker", "stop", TMP_CONTAINER_NAME])
-  result = subprocess.run(["docker", "rm", TMP_CONTAINER_NAME])
-  result.check_returncode()
-
-
-def get_app_file_list_string() -> str:
-  docker_cmd=['docker', 'exec', '-t', TMP_CONTAINER_NAME, 'bash', '-c', "find /tmp/XynaBlackEdition/components -iname \\*.app "]
-  app_paths=subprocess.check_output(docker_cmd, shell=True, text=True)
-  return app_paths
-
-
-def get_installed_app_names() -> str:
-  docker_cmd=['docker', 'exec', '-t', TMP_CONTAINER_NAME, 'bash', '-c', """awk -F= '($1=="installation.folder") { print $2 }'  /etc/opt/xyna/environment/black_edition_001.properties"""]
-  xyna_path = subprocess.check_output(docker_cmd, shell=True, text=True)
-  xyna_path = xyna_path.strip()
-  docker_cmd=['docker', 'exec', '-t', TMP_CONTAINER_NAME, 'bash', '-c', "cd " + xyna_path + "/server; ./xynafactory.sh status; while [[ $? -ne 0 ]]; do sleep 1s; ./xynafactory.sh status; done"]
-  subprocess.run(docker_cmd, shell=True, text=True, check=False)
-  docker_cmd=['docker', 'exec', '-t', TMP_CONTAINER_NAME, 'bash', '-c', xyna_path + "/server/xynafactory.sh listapplications | awk '(NR>1) { print $1 }'"]
-  app_names_str = subprocess.check_output(docker_cmd, shell=True, text=True)
-  app_names_str = app_names_str.replace("'", "")
-  app_names = app_names_str.splitlines()
-  return app_names
-
-
-def build_app_dict_from_paths(app_paths: str) -> dict:
-  applist = XynaAppList(paths = app_paths)
-  filter_app_names = get_installed_app_names()
-  applist.filter_apps(filter_app_names)
-  return applist.to_dict()
-
-
-def build_apps_to_install_string(applist: XynaAppList) -> str:
-  ret = ""
-  isfirst = True
-  for app in applist.applist:
-    if not app.install_flag:
-      continue
-    if isfirst:
-      isfirst = False
-    else:
-      ret += " && \\  \n"
-    ret += APP_LINE + app.name
-  if isfirst:
-    ret += "    echo 'no apps to install'"
-  ret += " && \\"
-  return ret
-
-
-def gen_prop_content(image_name: str) -> configparser.ConfigParser:
-  config = configparser.ConfigParser(allow_no_value=True)
-  config.optionxform = lambda option: option
-  firstline = PROP_FILE_HEADER.replace("###_BASE_IMAGE_###", image_name)
-  config[SECTION_NAME_HEADER] = {}
-  config.set(SECTION_NAME_HEADER, firstline)
-  config.set(SECTION_NAME_HEADER, PROP_NAME_BASE_IMAGE, image_name)
-  config.set(SECTION_NAME_HEADER, PROP_NAME_TARGET_IMAGE, PROP_VALUE_TARGET_IMAGE)
-  config[SECTION_NAME_APPS] = {}
-  config.set(SECTION_NAME_APPS, PROP_FILE_APP_BLOCK_START)
-  pathstr = get_app_file_list_string()
-  apps = build_app_dict_from_paths(pathstr)
-  config[SECTION_NAME_APP_LIST] = apps
-  return config
-
-
-def gen_prop_file(image_name: str, out_file: str):
-  print("Loading xyna base docker image to determine applications list...", flush=True)
-  start_xyna_base_container(image_name)
-  try:
-    content = gen_prop_content(image_name)
-  except:
-    print("Unexpected error:", sys.exc_info()[0])
-    drop_xyna_base_container()
-    raise
-  drop_xyna_base_container()
-  write_config_file(out_file, content)
-
-
-def gen_docker_file(prop_file: str, docker_file: str):
-  content = gen_docker_file_content(prop_file)
-  write_file(docker_file, content)
-
-
-def gen_apps_cmds_string(config: configparser.ConfigParser) -> str:
-  applist = XynaAppList(config = config)
-  apps = build_apps_to_install_string(applist)
-  return CMDS_APPS_TEMPL.replace("###_APP_CMD_LIST_###", apps)
-
-
-def gen_commands_string(config: configparser.ConfigParser) -> str:
-  commands = ""
-  apps_cmds = gen_apps_cmds_string(config)
-  commands += apps_cmds
-  ### insert additional command blocks here
-  return commands
-
-
-def gen_docker_file_content(prop_file: str) -> str:
-  config = configparser.ConfigParser(allow_no_value=True, comment_prefixes=(';'))
-  config.optionxform = lambda option: option
-  config.read(prop_file)
-  header = config[SECTION_NAME_HEADER]
-  base_image = header[PROP_NAME_BASE_IMAGE]
-  target_image = header[PROP_NAME_TARGET_IMAGE]
-  commands = gen_commands_string(config)
-  ret = DOCKER_TEMPLATE
-  ret = ret.replace("###_BASE_IMAGE_###", base_image)
-  ret = ret.replace("###_TARGET_IMAGE_###", target_image)
-  ret = ret.replace("###_ALL_COMMANDS_###", commands)
-  return ret
-
-
-def read_param(name: str) -> str:
-  if not name.startswith("-"):
-    name = "-" + name
-  arglist = sys.argv[1:]
-  matched = False
-  for arg in arglist:
-    if matched:
-      return arg
-    if arg == name:
-      matched = True
-  print("Expected parameter missing: " + name)
-  usage()
-
-
-def usage():
-  print("Usage: ")
-  print(sys.argv[0], " createPropertiesFile -input <image> -output <file>")
-  print(sys.argv[0], " createDockerfile -input <file> -output <file> ")
-  sys.exit(1)
-
-
 def main():
-  if len(sys.argv) != 6:
-    usage()
-  variant = sys.argv[1]
-  if variant == "createPropertiesFile":
-    gen_prop_file(read_param("input"), read_param("output"))
-  elif variant == "createDockerfile":
-    gen_docker_file(read_param("input"), read_param("output"))
-  else:
-    usage()
+  parser = argparse.ArgumentParser()
+  subparsers = parser.add_subparsers(dest='commands')
+  parser_prop = subparsers.add_parser('createPropertiesFile')
+  parser_prop.add_argument('-input', help='Name of existing docker image of xyna factory')
+  parser_prop.add_argument('-output', help='Name of property file to create')
+
+  parser_dock = subparsers.add_parser('createDockerfile')
+  parser_dock.add_argument('-input', help='Name of existing property file')
+  parser_dock.add_argument('-output', help='Name of docker file to create')
+
+  args = parser.parse_args()
+  if args.commands == "createPropertiesFile":
+    builder = PropertyFileBuilder(args.input)
+    builder.execute(args.output)
+  elif args.commands == "createDockerfile":
+    builder = DockerfileBuilder()
+    builder.execute(args.input, args.output)
 
 
 main()

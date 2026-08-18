8f27968d41f191e10e07a1034e51ed1633c8a3bc
Lukas Fey <107985579+LukasFey-GIP@users.noreply.github.com>
2026-08-18 10:07:02 +0200

    132 the java call in queuesh does not open javaio (#133)
    * fix java calls in queue.sh not opening java.io

--- xynabase/k8s/xyna/queue.sh
+++ xynabase/k8s/xyna/queue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Copyright 2024 Xyna GmbH, Germany
+# Copyright 2026 Xyna GmbH, Germany
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -140,7 +140,7 @@ for PREFIX in "${PREFIXES[@]}"; do
             SERVERLIBS+=$SERVERLIBFILEPATH":"
         done
         # process and set CONNECTDATA
-        PROCESSEDCONNECTDATA=$(java -classpath $SERVERLIBS com.gip.xyna.xmcp.xfcli.scriptentry.CreateBlobbedQueueData $QUEUETYPE $CONNECTDATA )
+        PROCESSEDCONNECTDATA=$(java --add-opens java.base/java.io=ALL-UNNAMED -classpath $SERVERLIBS com.gip.xyna.xmcp.xfcli.scriptentry.CreateBlobbedQueueData $QUEUETYPE $CONNECTDATA )
         if [ $? != 0 ] ; then
           echo "$0 ERROR: Could not process connectdata."
           echo "$0        QueueType: $QUEUETYPE"
@@ -161,7 +161,7 @@ for PREFIX in "${PREFIXES[@]}"; do
             SERVERLIBS+=$SERVERLIBFILEPATH":"
         done
         # process and set QUEUETYPE
-        PROCESSEDQUEUETYPE=$(java -classpath $SERVERLIBS com.gip.xyna.xmcp.xfcli.scriptentry.CreateBlobbedQueueData $QUEUETYPE)
+        PROCESSEDQUEUETYPE=$(java --add-opens java.base/java.io=ALL-UNNAMED -classpath $SERVERLIBS com.gip.xyna.xmcp.xfcli.scriptentry.CreateBlobbedQueueData $QUEUETYPE)
         NEWLINE=$(echo "$QUEUETYPELINE" | sed "s#<queueData>.*</queueData>#<queueData>$PROCESSEDQUEUETYPE</queueData>#")
         sed -i "$QUEUETYPEIDX s#.*#$NEWLINE#" "$XMLFILEPATH"
         echo "$0 INFO: set queuetype for queue $PREFIX"
